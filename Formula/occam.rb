ed8903a7c83f1836a351b8fca2a822ac9d650d18
millia ampora <198710911+dappermint@users.noreply.github.com>
2026-08-27 22:07:47 +0400

    chore: point the in-repo formula at v0.2.3

--- Formula/occam.rb
+++ Formula/occam.rb
@@ -1,8 +1,8 @@
 class Occam < Formula
   desc "Control a Razer BlackShark V3 Pro from macOS, without Synapse"
   homepage "https://github.com/dappermint/occam"
-  url "https://github.com/dappermint/occam/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "7072d8c23214eb1b3928a31d10223a86ea50eed5b5f51b798a7b713b8d760f43"
+  url "https://github.com/dappermint/occam/archive/refs/tags/v0.2.3.tar.gz"
+  sha256 "88bbe3364dbbfc23488536ac3a285f7f1b1bb2a1b1344bdd6d6bdf8beabc9546"
   license "MIT"
   head "https://github.com/dappermint/occam.git", branch: "main"
 
