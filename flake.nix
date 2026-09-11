864e8443fd27f3f30d8f3ca75ddf9b45b0df3da2
millia ampora <198710911+dappermint@users.noreply.github.com>
2026-09-11 06:50:10 +0400

    chore: stamp v0.4.5

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
         let
           occam = pkgs.buildGoModule {
             pname = "occam";
-            version = "0.4.4";
+            version = "0.4.5";
             src = lib.fileset.toSource {
               root = ./.;
               fileset = lib.fileset.unions [
@@ -35,7 +35,7 @@
             ldflags = [
               "-s"
               "-w"
-              "-X github.com/dappermint/occam/cmd.version=0.4.4"
+              "-X github.com/dappermint/occam/cmd.version=0.4.5"
             ];
             meta = {
               description = "native macOS control of a Razer BlackShark V3 Pro, without Synapse";
